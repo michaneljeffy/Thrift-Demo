f5409f0e70ad948b17539236cafacfde88a88264
jinghong <jinghong@in66.com>
2017-11-10 15:20:41 +0800

    first commit

---
+++ Thrift.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace Thrift.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TTransport transport = new TSocket("localhost",9095);
+            transport.Open();
+
+            TProtocol protocal = new TBinaryProtocol(transport);
+            MathService.Client client = new MathService.Client(protocal);
+
+            Console.WriteLine(client.add (10,20));
+            Console.WriteLine(client.sub (10, 20));
+            Console.WriteLine(client.mul(10, 20));
+            Console.WriteLine(client.div(10, 20));
+            Console.WriteLine(client.mod(10, 20));
+        }
+    }
+}
---
+++ Thrift.Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Server;
+using Thrift.Transport;
+using Thrift.Protocol;
+
+namespace Thrift.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MathServer handler = new MathServer();
+                MathService.Processor process = new Server.MathService.Processor(handler);
+
+                TServerTransport serverTransport = new TServerSocket(9095);
+                TServer server = new TSimpleServer(process, serverTransport);
+                Console.WriteLine("Starting the server....");
+                server.Serve();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
